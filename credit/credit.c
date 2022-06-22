4062bc15ff5fa124cf6ab00d0ca1e4a73ec67fbc
Jordan Clayton <jordanclaytonvisualart@gmail.com>
2022-06-22 18:22:41 -0400

    fixed and rewrote; now works with most cases & solves CS50's credit

--- credit/credit.c
+++ credit/credit.c
@@ -1,65 +1,101 @@
 #include<stdio.h>
 #include <stdbool.h>
 
-// finish later.
-// Get user input for a long.
-//Verify with checksum: n%10, n/=10 loop. LUHN's ALGO.
-//If invalid, return.
-//Else:
-//FIGURE out how to get the first digits of a long.
-//Check for VISA/AMEX/MC
-//Return.
-//Print info to console.
-long GetCardNumber();
-bool validateChecksum(long card);
-void PrintCard(bool valid, long card);
+long long GetCardNumber();
+bool ValidateChecksum(long long card, int *lead, int *numDigits);
+void PrintCard(bool valid, int card, int numDigits);
 
 int main(void){
-    int cardNumber = GetCardNumber();
-    bool isValid = validateChecksum(cardNumber);
-    printCard(isValid);
+    int leadingDigits = 0;
+    int ccNumDigits = 0;
+    long long cardNumber = GetCardNumber();
+    bool isValid = ValidateChecksum(cardNumber, &leadingDigits, &ccNumDigits);
+    PrintCard(isValid, leadingDigits, ccNumDigits);
     return 0;
 }
-long GetCardNumber(){
+long long GetCardNumber(){
     //This doesn't handle invalid numbers yet.
     //Solution will use <cs50.h>, and I do not know
-    //How to check for type in C yet.
-    long cardNum;
+    //How to check for input type in C yet.
+    long long cardNum;
     printf("Number: ");
-    scanf("%li", & cardNum);
+    scanf("%lld", &cardNum);
     return cardNum;
 
 }
 
-bool validateChecksum(long card){    
-    long n = card;
+bool ValidateChecksum(long long card, int *lead, int *numDigits){    
+    long long n = card;
     int doubledSum = 0;
     int singlesSum = 0;
     bool isDoubled = false;
+    int counter = 0;
     //Luhn Algo.
     do{
         int digit = n%10;
+        
         if(isDoubled){
-            doubledSum += (digit*2);
+            int doubledDigit = digit*2;
+            if(doubledDigit >= 10){
+                do{
+                    doubledSum += (doubledDigit % 10);
+                    doubledDigit /= 10;
+                }while(doubledDigit > 0);
+            }else{
+                doubledSum += doubledDigit;
+            }
         }else{
             singlesSum += digit;
         }
-        n /=10;
+        n /=10LL;
         isDoubled = !isDoubled;
+        counter++;
 
+        //Grabbing the leading digits.
+        //Not all mastercards start with 5, but for this problem, ignoring.
+        if(n < 57L &&  n > 30L){
+            *lead = (int)n;
+        }    
     }while(n > 0);
     int checkSum = doubledSum + singlesSum;
+    *numDigits = counter;   
     if(checkSum % 10 == 0){
+        
         return true;
     }
     return false;
 }
-void PrintCard(bool valid, long card){
-    if(!valid){
+void PrintCard(bool valid, int card, int numDigits){
+    //If the card fails the checksum or the leading digits do not match,
+    //guard clause to escape early & avoid extra conditions check.
+    //Obviously foregoing the fact that some mastercards can start with 2.
+    if(!valid || card < 30){
         printf("INVALID");
-        return 0;
-    }else{
-        printf("VALID CARD");
+        return;
     }
-    //FIGURE OUT THIS PART.
+
+
+    //Valid checksum & card has proper leading digits:
+    //Compare leading with expected number of CC digits.
+
+    //MC check
+    if(card > 50 && card < 57 && numDigits == 16){
+        printf("MASTERCARD");
+
+    //Visa check
+
+    }else if(card > 39 && card < 50 && (numDigits == 13 || numDigits == 16)){
+        printf("VISA");
+
+    //AMEX check
+
+    }else if((card == 34 || card == 37) && numDigits == 15)
+    {
+        printf("AMEX");
+
+    //If all of these fail, assume an invalid card.
+    }else{
+        printf("INVALID");
+    }
+    return;
 }
